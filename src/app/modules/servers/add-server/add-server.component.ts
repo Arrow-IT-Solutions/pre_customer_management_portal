74fc606f968164268a076ed1614e8b0bb9b81752
Nour <noursaleem@arrowitjo.com>
2025-07-05 20:44:19 +0300

    add servers page + edit icons in all pages

--- src/app/modules/servers/add-server/add-server.component.ts
+++ src/app/modules/servers/add-server/add-server.component.ts
@@ -1,43 +1,35 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
-import { DataBasesService } from 'src/app/layout/service/databases.service';
 import { LayoutService } from 'src/app/layout/service/layout.service';
+import { ServersService } from 'src/app/layout/service/servers.service';
 
 @Component({
-  selector: 'app-add-database',
-  templateUrl: './add-database.component.html',
-  styleUrls: ['./add-database.component.scss'],
+  selector: 'app-add-server',
+  templateUrl: './add-server.component.html',
+  styleUrls: ['./add-server.component.scss'],
   providers: [MessageService]
 })
-export class AddDatabaseComponent {
+export class AddServerComponent {
         dataForm!: FormGroup;
         submitted: boolean = false;
         btnLoading: boolean = false;
         loading: boolean = false;
-        isPasswordVisible: boolean = false;
         constructor(public formBuilder:FormBuilder,
           public messageService: MessageService,
-          public database:DataBasesService,
+          public server: ServersService,
           public layoutService: LayoutService,
   
         ){
           this.dataForm=this.formBuilder.group({
-            nameEn:['',Validators.required],
-            nameAr:['',Validators.required],
-            userName:['',Validators.required],
-            password:['',Validators.required]
-            
+            hostName:['',Validators.required],
+            IpAddress:['',Validators.required],
+           
           })
         }
         get form(): { [key: string]: AbstractControl } {
         return this.dataForm.controls;
-        }
-         togglePasswordVisibility(): void {
-           this.isPasswordVisible = !this.isPasswordVisible;
-                 }
-
-
+      }
        async ngOnInit() {}
        async onSubmit() {}
        async Save() {}
---
+++ src/app/modules/servers/servers.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ServersRoutingModule } from './servers-routing.module';
+import { ServersComponent } from './servers/servers.component';
+import { AddServerComponent } from './add-server/add-server.component';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ResponseBase } from 'src/app/shared/class/class';
+
+
+@NgModule({
+  declarations: [
+    ServersComponent,
+    AddServerComponent
+  ],
+  imports: [
+    CommonModule,
+    ServersRoutingModule,
+    SharedModule,
+    ReactiveFormsModule
+  ]
+})
+export class ServersModule { }
+export interface ServersResponse extends ResponseBase {
+  
+}
