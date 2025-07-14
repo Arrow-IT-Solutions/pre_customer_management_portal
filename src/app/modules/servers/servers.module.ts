06b953fd4f6b2bebb0b3981b5feca040fdf10350
HasanHmoudDAS <HasanHmoud@DigiArenas.com>
2025-07-15 00:39:31 +0300

    Server portal work

--- src/app/modules/servers/servers.module.ts
+++ src/app/modules/servers/servers.module.ts
@@ -6,8 +6,7 @@ import { ServersComponent } from './servers/servers.component';
 import { AddServerComponent } from './add-server/add-server.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
-import { ResponseBase } from 'src/app/shared/class/class';
-
+import { RequestBase, ResponseBase, SearchRequestBase } from 'src/app/shared/class/class';
 
 @NgModule({
   declarations: [
@@ -22,6 +21,27 @@ import { ResponseBase } from 'src/app/shared/class/class';
   ]
 })
 export class ServersModule { }
-export interface ServersResponse extends ResponseBase {
-  
+
+export interface ServerRequest {
+  uuid?: string;
+  hostname?: string;
+  ipAddress?: string;
+}
+
+export interface ServerUpdateRequest {
+  uuid?: string;
+  hostname?: string;
+  ipAddress?: string;
+}
+
+export interface ServerSearchRequest extends RequestBase {
+  uuid?: string;
+  hostname?: string;
+  ipAddress?: string;
+}
+
+export interface ServerResponse extends ResponseBase {
+  uuid?: string;
+  hostname?: string;
+  ipAddress?: string;
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   //baseApiUrl: "https://survey-api-dev.apps-jo.com"
-  baseApiUrl: "https://localhost:52995"
+  baseApiUrl: "https://localhost:52009"
 };
 
 //https://survey-api-dev.apps-jo.com
